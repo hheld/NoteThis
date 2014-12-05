88bb4b6fe14f85520bd8785e08c56128652b7438
Harald Held <harald.held@gmail.com>
2014-12-05 22:53:08 +0100

    Simple editor works now.

---
+++ server/server.js
@@ -0,0 +1,16 @@
+/*jslint node: true, nomen: true, es5: true */
+'use strict';
+
+var port = process.argv[2] || 8000;
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+app.use(express.static(__dirname + '/../node_modules'));
+app.use(express.static(__dirname + '/../node_modules/bootstrap/dist'));
+app.use(express.static(__dirname + '/../bower_components'));
+app.use(express.static(__dirname + '/../client'));
+
+http.listen(port, function () {
+    console.log('Started web server on port ' + port);
+});
