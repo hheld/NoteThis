17df1cca0c849f64685e704959112c9667798d6c
Harald Held <harald.held@gmail.com>
2014-12-16 21:59:08 +0100

    Like this, it also works with node-webkit.

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 /* jshint node: true */
-'use strict';
+module.exports = (function () {
+    'use strict';
 
-var port = process.argv[2] || 8000;
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
+    var port = process.argv[2] || 8000;
+    var express = require('express');
+    var app = express();
+    var http = require('http').Server(app);
 
-app.use(express.static(__dirname + '/../node_modules'));
-app.use(express.static(__dirname + '/../node_modules/bootstrap/dist'));
-app.use(express.static(__dirname + '/../bower_components'));
-app.use(express.static(__dirname + '/../client'));
+    app.use(express.static(__dirname + '/../node_modules'));
+    app.use(express.static(__dirname + '/../node_modules/bootstrap/dist'));
+    app.use(express.static(__dirname + '/../bower_components'));
+    app.use(express.static(__dirname + '/../client'));
 
-http.listen(port, function () {
-    console.log('Started web server on port ' + port);
-});
+    http.listen(port, function () {
+        console.log('Started web server on port ' + port);
+    });
+}());
