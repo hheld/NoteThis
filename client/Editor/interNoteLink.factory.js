c6cfb801eb10c93fd04f399a3bedd1908e3620cf
Harald Held <harald.held@gmail.com>
2014-12-20 14:55:49 +0100

    Links to notes with spaces works now.

--- client/Editor/interNoteLink.factory.js
+++ client/Editor/interNoteLink.factory.js
@@ -17,7 +17,39 @@
         return service;
 
         function noteLinksFixer (noteText) {
-            return noteText.replace(/\[\[(.*)\]\]/g, '[$1](/#/viewNote/$1)');
+            var linkRegexp = /\[\[(.*)\]\]/g;
+            var linkMatch = linkRegexp.exec(noteText);
+            var linksToBeFixed = [];
+
+            if(linkMatch) {
+                linksToBeFixed.push({
+                    linkMatch: linkMatch[0],
+                    fixedLink: getFixedLink(linkMatch[1])
+                });
+            }
+
+            while (linkMatch) {
+                linkMatch = linkRegexp.exec(noteText);
+
+                if (linkMatch) {
+                    linksToBeFixed.push({
+                        linkMatch: linkMatch[0],
+                        fixedLink: getFixedLink(linkMatch[1])
+                    });
+                }
+            }
+
+            angular.forEach(linksToBeFixed, function (link) {
+                noteText = noteText.replace(link.linkMatch, link.fixedLink);
+            });
+
+            return noteText;
+        }
+
+        function getFixedLink (linkName) {
+            var fixedLink = linkName.replace(' ', '%20');
+
+            return '[' + linkName + '](/#/viewNote/' + fixedLink + ')';
         }
     }
 })();
